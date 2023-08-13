816906ea1ae3d692eb7929b8f863cc4303eb8034
SAS-Acidness <142126208+SAS-Acidness@users.noreply.github.com>
2023-08-12 20:20:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function display(form){
+    if (form.username.value=="Julia Maas") { 
+            if (form.password.value=="Sashaisbae32") {              
+                                            location="/index3.html" 
+                                                } else {
+                                                        document.getElementById("text_one").innerHTML = "Invalid Username/Password!";
+                                                        }
+                                    } else {  document.getElementById("text_one").innerHTML = "Invalid Username/Password!";
+                                            }
+                            }
---
+++ script2.js
@@ -0,0 +1,10 @@
+function display(form){
+    if (form.username.value=="Tiger") { 
+            if (form.password.value=="Jewels1236") {              
+                                            location="/index2.html" 
+                                                } else {
+                                                        document.getElementById("text_one").innerHTML = "Invalid Username/Password!";
+                                                        }
+                                    } else {  document.getElementById("text_one").innerHTML = "Invalid Username/Password!";
+                                            }
+                            }
